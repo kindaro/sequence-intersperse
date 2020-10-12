163cfab3fe82c7e81c3e5200ddcea87ab22049f6
Ignat Insarov <kindaro@gmail.com>
2020-10-12 16:37:33 +0500

    Elaborate benchmarks.

--- Main.hs
+++ Main.hs
@@ -1,15 +1,44 @@
 module Main where
 
 import Prelude.Unicode
+import Data.Foldable
 import qualified Function
 import qualified Applicative
+import Data.Sequence (Seq)
 import qualified Data.Sequence as Sequence
+import qualified Data.List as List
 import Data.Char
+import GHC.Exts (fromList, IsList, Item)
 
-n = 2^22
+n = 2^20
+
+printableCharacters = filter isPrint [minBound.. maxBound]
+
+xs = (take n ∘ cycle) printableCharacters
+
+data Benchmark = forall f. (IsList (f Char), Functor f, Foldable f, Item (f Char) ~ Char) ⇒ Benchmark (f Char → f Char)
+
+force ∷ (Foldable f, Functor f) ⇒ f Char → IO ( )
+force = (print ∘ sum ∘ fmap ord)
 
 main = do
-  let xs = (Sequence.cycleTaking n ∘ Sequence.fromList ∘ filter isPrint) [minBound.. maxBound]
-  (print ∘ sum ∘ fmap ord) xs
-  {-# SCC function #-} (print ∘ sum ∘ fmap ord) (Function.intersperse '\NUL' xs)
-  {-# SCC applicative #-} (print ∘ sum ∘ fmap ord) (Applicative.intersperse '\NUL' xs)
+  traverse_ benchmark
+    [Benchmark (id @[Char])
+    , Benchmark (id @(Seq Char))
+    , Benchmark stock
+    , Benchmark function
+    , Benchmark applicative
+    , Benchmark list
+    ]
+
+benchmark ∷ Benchmark → IO ( )
+benchmark (Benchmark f) = force $ (f ∘ fromList) xs
+{-# inline benchmark #-}
+
+applicative = Applicative.intersperse '\NUL'
+
+function = Function.intersperse '\NUL'
+
+stock = Sequence.intersperse '\NUL'
+
+list = List.intersperse '\NUL'
